8be02e7504cca37afff9d296fa759923b21ba501
Denny Setyawan Huda <denycam9@gmail.com>
2023-06-04 10:59:21 +0700

    refactor: implemen render card

---
+++ src/data/index.ts
@@ -0,0 +1,74 @@
+export const destinations = [
+  {
+    title: "Pura Tanah Lot",
+    imageUrl:
+      "https://anekatempatwisata.com/wp-content/uploads/2014/10/pura-Tanah-Lot-610x453.jpg",
+    top: true,
+  },
+  {
+    title: "Pantai Padang Padang",
+    imageUrl:
+      "https://anekatempatwisata.com/wp-content/uploads/2014/11/Pantai-Padang-Padang.jpg",
+    top: true,
+  },
+  {
+    title: "Danau Beratan Bedugul",
+    imageUrl:
+      "https://anekatempatwisata.com/wp-content/uploads/2014/03/Danau-Beratan-Bedugul.jpg",
+    top: true,
+  },
+  {
+    title: "Pura Besakih",
+    imageUrl:
+      "https://anekatempatwisata.com/wp-content/uploads/2014/03/Pura-Besakih.jpg",
+    top: true,
+  },
+  {
+    title: "Monkey Forest",
+    imageUrl:
+      "https://anekatempatwisata.com/wp-content/uploads/2015/11/Ubud-Monkey-Forest.jpg",
+    top: true,
+  },
+  {
+    title: "Pantai Kuta Bali",
+    imageUrl:
+      "https://anekatempatwisata.com/wp-content/uploads/2014/03/Pantai-Kuta-610x458.jpg",
+    top: true,
+  },
+  {
+    title: "Garuda Wisnu Kencana",
+    imageUrl:
+      "https://anekatempatwisata.com/wp-content/uploads/2014/03/Garuda-Wisnu-Kencana.jpg",
+    top: false,
+  },
+  {
+    title: "Pasar Sukawati",
+    imageUrl:
+      "https://anekatempatwisata.com/wp-content/uploads/2020/02/Pasar-Seni-Sukawati-610x343.jpg",
+    top: false,
+  },
+  {
+    title: "Pura Uluwatu",
+    imageUrl:
+      "https://anekatempatwisata.com/wp-content/uploads/2014/03/Pura-Uluwatu.jpg",
+    top: false,
+  },
+  {
+    title: "Monumen Bom Bali",
+    imageUrl:
+      "https://anekatempatwisata.com/wp-content/uploads/2020/02/Monumen-Bom-Bali.jpg",
+    top: false,
+  },
+  {
+    title: "Mall Beachwalk",
+    imageUrl:
+      "https://anekatempatwisata.com/wp-content/uploads/2020/02/Mall-Beach-Walk.jpg",
+    top: false,
+  },
+  {
+    title: "Pura Ulun Danu",
+    imageUrl:
+      "https://anekatempatwisata.com/wp-content/uploads/2020/02/Objek-Wisata-Pura-Ulun-Danu-Bedugul.jpg",
+    top: false,
+  },
+];
